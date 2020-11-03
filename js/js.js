6301281115571e60096a6f575705f815368fc7b3
Padmemesamidala <68656454+Padmemesamidala@users.noreply.github.com>
2020-11-02 22:14:47 -0600

    Revenge of The SIMP 1

---
+++ js/js.js
@@ -0,0 +1,55 @@
+//
+const btnDescargar = document.querySelector(".botonDescarga");
+console.log(btnDescargar);
+
+btnDescargar.addEventListener('click', () => {
+    alert("Lo sentimos, la pelicula aún no esta disponible, pero ¡esperala muy pronto amigo!");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function alerta1() {
+    //Aqui escribiré la funcion para cuando el usuario de click al boton de descargar
+    alert("Lo sentimos, la pelicula aún no esta disponible, pero !esperala muy pronto amigo!");
+}
+
+*/
+
+
+
+/*
+btnDescargar.onclick = function() {
+    //Aqui escribiré la funcion para cuando el usuario de click al boton de descargar
+    alert("Lo sentimos, la pelicula aún no esta disponible, pero !esperala muy pronto amigo!");
+}*/
